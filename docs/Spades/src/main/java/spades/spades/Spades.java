afabedc689d534c1abef86e4709c65b6bbe0e8b4
djmitch44 <112188600+djmitch44@users.noreply.github.com>
2022-11-06 23:53:21 -0600

    Add files via upload

---
+++ docs/Spades/src/main/java/spades/spades/Spades.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package spades.spades;
+
+/**
+ *
+ * @author hinto
+ */
+public class Spades {
+
+    public static void main(String[] args) {
+        StartScreen ss = new StartScreen();
+        ss.setVisible(true);
+    }
+}
